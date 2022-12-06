24351d717ea43690df953baf5f4e6fde969561bc
Michael Anderson <adrien.spy@gmail.com>
2022-12-06 14:42:30 +0100

    Dynamic links

--- common/includes/header.php
+++ common/includes/header.php
@@ -1,4 +1,10 @@
 	<!-- En-tête  -->
 	<header>
-		<a href="/"><h1>WiChat</h1></a> 
+		<?php
+		if ($_SERVER['PHP_SELF'] == "/index.php") {
+			echo '<a href="#"><h1>WiChat</h1></a>';
+		} else {
+			echo '<a href="/"><h1>WiChat</h1></a>';
+		}
+		?>
 	</header>
