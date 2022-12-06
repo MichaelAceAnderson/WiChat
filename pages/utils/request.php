08fc3a96a17c8f809e179012b671ae84b952886a
Michael Anderson <adrien.spy@gmail.com>
2022-12-06 14:42:14 +0100

    Request optimization & Exceptions

--- pages/utils/request.php
+++ pages/utils/request.php
@@ -16,27 +16,35 @@ if (isset($_GET['display'])) {
 		exit();
 	}
 
-	// Définition de la requête
-	$request = "SELECT messages.chatId, users.username, messages.message, messages.timestamp 
+	try {
+		// Exécution de la requête
+		$result = $dbConnection->query(
+			"SELECT messages.chatId, users.username, messages.message, messages.timestamp 
 		FROM messages JOIN users 
 		ON messages.authorId=users.id 
-		WHERE chatId = '" . $chatId . "'";
-
-	// Exécution de la requête
-	$result = $dbConnection->query($request);
-
-	if ($result) {
-		// Si la requête renvoie bien un résultat quel qu'il soit
-		//Affichage des résultats
-		while ($row = $result->fetch_array()) {
-			echo '<div class="msg in">
+		WHERE chatId = '" . $chatId . "'
+		"
+		);
+		if (!$result) {
+			throw new Exception("La boîte de réception ne répond pas correctement");
+		} else {
+			// Si la requête renvoie bien un résultat quel qu'il soit
+			//Affichage des résultats
+			while ($row = $result->fetch_array()) {
+				echo '<div class="msg in">
 			<div class="chat-id">' . $row["chatId"] . '</div>
 			<div class="username">' . $row["username"] . '</div>
 			<div class="content">' . $row["message"] . '</div>
 			<div class="timestamp">' . $row["timestamp"] . '</div>
 			</div>';
+			}
 		}
+	} catch (Exception $e) {
+		//Si la connexion a échoué et que $dbConnection a lancé (throw) une erreur
+		die('Impossible de joindre le serveur de tchat: ' . $e->getMessage());
+		exit();
 	}
+
 	//Fermeture de la base de données
 	$dbConnection->close();
 }
@@ -54,14 +62,24 @@ if (isset($_GET['send'])) {
 	}
 	// Décoder les données envoyées par axios.post
 	$_POST = json_decode(file_get_contents("php://input"), true);
-	if (isset($_POST['msg'])) {
-
-		// Exécution de la requête d'insertion
-		$result = $dbConnection->query("
-		INSERT INTO messages(chatId, authorId, message)
-		VALUES (0, " . $_POST['userId'] . ", '" . $_POST['msg'] . "') 
-		");
+	if (isset($_POST['msg']) && $_POST['msg'] != "") {
+		try {
+			// Exécution de la requête d'insertion
+			$result = $dbConnection->query("
+			INSERT INTO messages(chatId, authorId, message)
+			VALUES (0, " . $_POST['userId'] . ", '" . $_POST['msg'] . "') 
+			");
+			if (!$result) {
+				throw new Exception("L'envoi de message a échoué");
+			}
+		} catch (Exception $e) {
+			//Si la connexion a échoué et que $dbConnection a lancé (throw) une erreur
+			die('Impossible de joindre le serveur de tchat: ' . $e->getMessage());
+			exit();
+		}
+		//Fermeture de la connexion à la base de données
+		$dbConnection->close();
+	} else {
+		echo "Vous devez saisir un message !";
 	}
-	//Fermeture de la connexion à la base de données
-	$dbConnection->close();
 }
