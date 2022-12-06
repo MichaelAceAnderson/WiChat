1b2e7743206257512668dc505017fc4045f97aa8
Michael Anderson <adrien.spy@gmail.com>
2022-12-06 14:42:52 +0100

    Error design fix

--- common/js/axiosUpdate.js
+++ common/js/axiosUpdate.js
@@ -1,64 +1,66 @@
 window.addEventListener("load", () => {
-    // Mettre à jour le tchat toutes les secondes
-    setInterval(() => { updatePage(); }, 1000);
-    // Écoute sur la saisie de message
-    document.getElementsByClassName("msg-input")[0].addEventListener("keydown", (msgInput) => {
-        // Si l'utilisateur appuie sur Entrée
-        if (msgInput.key == "Enter") {
-            // Appeler sendMSg pour envoyer la saisie en base de données
-            sendMsg(msgInput);
-        }
-    }, false);
+	// Mettre à jour le tchat toutes les secondes
+	setInterval(() => { updatePage(); }, 1000);
+	// Écoute sur la saisie de message
+	document.getElementsByClassName("msg-input")[0].addEventListener("keydown", (msgInput) => {
+		// Si l'utilisateur appuie sur Entrée
+		if (msgInput.key == "Enter") {
+			// Appeler sendMSg pour envoyer la saisie en base de données
+			sendMsg(msgInput);
+		}
+	}, false);
 }, false);
 
 // Fonction de mise à jour du tchat
 function updatePage() {
-    let chatBox = document.getElementById('chatBox')
-    axios.get('/pages/utils/request.php?display')
-        .then(response => {
-            let pageResponse = response.data;
-            if (!pageResponse.includes("Impossible")) {
-                // Si la réponse ne comprend pas un message "impossible"
-                chatBox.innerHTML = pageResponse;
-                // Mettre le curseur en bas
-                chatBox.scrollTop = chatBox.scrollHeight
-                // Rendre visible le champ de saisie de message
-                document.getElementsByClassName("msg-input")[0].classList.add("visible");
-            }
-            else {
-                // Envoyer une erreur s'il y a lieu
-                throw new Error(response.data);
-            }
-        })
-        .catch((error) => {
-            // Gérer l'erreur
-            console.error(error.message); //Note: error.stack ajoute la provenance de la ligne de code fautive
-            // Cacher la saisie de message
-            document.getElementsByClassName("msg-input")[0].classList.remove("visible");
-            // Cacher la boîte de réception et afficher un message d'erreur
-            chatBox.innerHTML =
-                `<div class="error outlined">
-                    Impossible de joindre le serveur de tchat pour le moment.
-                    <br>
-                    Tentative de reconnexion... <img src="/common/img/loading.gif" height="30" style="vertical-align: middle;">
-                </div>`;
-        });
+	let chatBox = document.getElementById('chatBox')
+	axios.get('/pages/utils/request.php?display')
+		.then(response => {
+			let pageResponse = response.data;
+			if (!pageResponse.includes("Impossible")) {
+				// Si la réponse ne comprend pas un message "impossible"
+				chatBox.innerHTML = pageResponse;
+				// Mettre le curseur en bas
+				chatBox.scrollTop = chatBox.scrollHeight
+				// Rendre visible le champ de saisie de message
+				document.getElementsByClassName("msg-input")[0].classList.add("visible");
+			}
+			else {
+				// Envoyer une erreur s'il y a lieu
+				throw new Error(response.data);
+			}
+		})
+		.catch((error) => {
+			// Gérer l'erreur
+			console.error(error.message); //Note: error.stack ajoute la provenance de la ligne de code fautive
+			// Cacher la saisie de message
+			document.getElementsByClassName("msg-input")[0].classList.remove("visible");
+			// Cacher la boîte de réception et afficher un message d'erreur
+			chatBox.innerHTML =
+				`<div class="error outlined">
+					<p>
+						Impossible de joindre le serveur de tchat pour le moment.
+						<br>
+						Tentative de reconnexion... <img src="/common/img/loading.gif" height="30" style="vertical-align: middle;">
+                    </p>
+				</div>`;
+		});
 
 };
 
 
 function sendMsg(event) {
-    // Récupérer la valeur du champ de saisie sur lequel il y a eu écoute
-    msgToSend = event.target.value
-    // Envoi d'une requête POST pour envoyer le message
-    axios.post('/pages/utils/request.php?send', {
-        msg: msgToSend,
-        userId: 1
-    })
-        .then(function (response) {
-            updatePage();
-        })
-        .catch(function (error) {
-            console.error(error);
-        });
+	// Récupérer la valeur du champ de saisie sur lequel il y a eu écoute
+	msgToSend = event.target.value
+	// Envoi d'une requête POST pour envoyer le message
+	axios.post('/pages/utils/request.php?send', {
+		msg: msgToSend,
+		userId: 1
+		})
+		.then(function (response) {
+			updatePage();
+		})
+		.catch(function (error) {
+			console.error(error);
+		});
 }
--- pages/error.php
+++ pages/error.php
@@ -1,75 +1,79 @@
 <!DOCTYPE html>
 <html xml:lang="fr" xmlns="http://www.w3.org/1999/xhtml" lang="fr">
-<?php include_once("".$_SERVER['DOCUMENT_ROOT']."/common/includes/head.php"); ?>
-<?php include_once("".$_SERVER['DOCUMENT_ROOT']."/common/includes/header.php"); ?>
-	<body>
-		<META http-equiv="refresh" content="5; URL=/">
-		<?php header("Refresh:5; url=/"); ?>
-			<center class="main outlined">
-				<h1 class="response error">
-				<?php
-				echo "Erreur ".$_GET['error'].": <br>";
-				switch($_GET['error'])
-				{
-					case '400':
+<?php include_once("" . $_SERVER['DOCUMENT_ROOT'] . "/common/includes/head.php"); ?>
+<?php include_once("" . $_SERVER['DOCUMENT_ROOT'] . "/common/includes/header.php"); ?>
+
+<body>
+	<?php
+	// Redirection
+	echo '<META http-equiv="refresh" content="5; URL=/">';
+	header("Refresh:5; url=/");
+	?>
+	<center class="main outlined">
+		<h1 class="error">
+			<?php
+			echo "<p>Erreur " . $_GET['error'] . ": <br>";
+			switch ($_GET['error']) {
+				case '400':
 					echo 'Échec de l\'analyse HTTP !';
 					break;
 
-					case '401':
+				case '401':
 					echo 'Le pseudo ou le mot de passe n\'est pas correct !';
 					break;
 
-					case '402':
+				case '402':
 					echo 'Le client doit reformuler sa demande avec les bonnes données de paiement!';
 					break;
 
-					case '403':
+				case '403':
 					echo 'Acces interdit !';
 					break;
 
-					case '404':
+				case '404':
 					echo 'La page n\'existe pas ou plus !';
 					break;
 
-					case '405':
+				case '405':
 					echo 'Méthode non autorisée!';
 					break;
 
-					case '406':
+				case '406':
 					echo 'La requête n\'a pas pu aboutir a temps !';
 					break;
 
-					case '500':
+				case '500':
 					echo 'Erreur interne au serveur ou serveur saturé !';
 					break;
 
-					case '501':
+				case '501':
 					echo 'Le serveur ne supporte pas le service demandé !';
 					break;
 
-					case '502':
+				case '502':
 					echo 'Mauvaise passerelle !';
 					break;
 
-					case '503':
+				case '503':
 					echo ' Service indisponible !';
 					break;
 
-					case '504':
+				case '504':
 					echo 'Trop de temps à la réponse !';
 					break;
 
-					case '505':
+				case '505':
 					echo 'Version HTTP non supportée! ';
 					break;
 
-					default:
+				default:
 					echo 'Erreur inconnue !';
-				}
-				echo '<br>Redirection...';
+			}
+			echo '<br>Redirection...</p>';
 
-				?>
-			</h1>
-		</center>
-	</body>
+			?>
+		</h1>
+	</center>
+</body>
+
 </html>
