cfe3d8956763f0cc498b56a321492357f3cf6b49
Michael Anderson <adrien.spy@gmail.com>
2023-03-27 11:07:24 +0200

    About section fix

--- view/pages/about.php
+++ view/pages/about.php
@@ -8,13 +8,13 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/view/includes/header.php');
 
 <?php
 if (isset($_POST["submit"])) {
-	if (empty($_POST["mail"])) $error = "L'adresse mail ne peut pas être vide !";
-	elseif (empty($_POST["msg"])) $error = "Le message doit comporter un contenu !";
-	else {
-		$mail = $_POST["mail"];
-		$msg = $_POST["msg"];
-		$error = false;
-	}
+    if (empty($_POST["mail"])) $error = "L'adresse mail ne peut pas être vide !";
+    elseif (empty($_POST["msg"])) $error = "Le message doit comporter un contenu !";
+    else {
+        $mail = $_POST["mail"];
+        $msg = $_POST["msg"];
+        $error = false;
+    }
 }
 ?>
 
@@ -26,37 +26,37 @@ if (isset($_POST["submit"])) {
 
     <div class="content">
         <p class="outlined">
-            Ce site a été réalisé entièrement avec HTML5, CSS3, JavaScript ES 2020. Sans Framework. À la mano. Et ça
-            c'est la classe.
+            Ce site a été réalisé entièrement avec HTML5, CSS3, PHP8 JavaScript ES 2020. Sans Framework. À la mano.
+            Et ça c'est la classe.
         </p>
 
         <form action="#" method="post" class="contact">
             <h1>Envoyer un message</h1>
             <?php
-			if (isset($_POST["submit"])) {
-				if (empty($_POST["mail"])) {
-					echo	'<div class="response error">
+            if (isset($_POST["submit"])) {
+                if (empty($_POST["mail"])) {
+                    echo    '<div class="response error">
 										L\'adresse mail ne peut pas être vide !
 									</div>';
-				} elseif (empty($_POST["msg"])) {
-					echo	'<div class="response error">
+                } elseif (empty($_POST["msg"])) {
+                    echo    '<div class="response error">
 										Le message doit comporter un contenu !
 									</div>';
-				} else {
-					$mail = $_POST["mail"];
-					//Ajouter traitement regEx mail
-					$msg = $_POST["msg"];
+                } else {
+                    $mail = $_POST["mail"];
+                    //Ajouter traitement regEx mail
+                    $msg = $_POST["msg"];
 
-					$msgFile = fopen($_SERVER['DOCUMENT_ROOT'] . "/common/files/message.txt", "a+") or die("Impossible d'envoyer le message");
-					fwrite($msgFile, $mail . ": " . $msg . "\n");
-					fclose($msgFile);
-					echo	'<div class="response success">
+                    $msgFile = fopen($_SERVER['DOCUMENT_ROOT'] . "/common/files/message.txt", "a+") or die("Impossible d'envoyer le message");
+                    fwrite($msgFile, $mail . ": " . $msg . "\n");
+                    fclose($msgFile);
+                    echo    '<div class="response success">
 										<h3>Votre adresse:</h3>' . htmlentities($mail) . '
 										<h3>Message envoyé:</h3>' . htmlentities($msg) . '
 									</div>';
-				}
-			}
-			?>
+                }
+            }
+            ?>
             <input type="text" name="mail" placeholder="Votre e-mail" />
             <textarea placeholder="Votre message" name="msg"></textarea>
             <button type="submit" name="submit">Envoyer</button>
